597868170680a9cabae6fe1b8d405e4be51c9b6c
pasxn <pasansanjula.android@gmail.com>
2021-07-09 17:10:38 +0530

    change

--- linked_lists/linkedlist_static.cpp
+++ linked_lists/linkedlist_static.cpp
@@ -6,21 +6,19 @@ typedef struct node {
 } Node;
 
 int main() {
-    Node* head;
-    Node n1 = {1, NULL};
-    Node n2 = {2, NULL};
-    Node n3 = {3, NULL};
+    Node* head = NULL;
+	Node n1 = {1, NULL};
+	Node n2 = {2, NULL};
+	Node n3 = {3, NULL};
+	Node n4 = {4, NULL};
 
-    head = &n1;
-    n1.next = &n2;
-    n2.next = &n3;
+	head = &n1;
+	n1.next = &n2;
+	n2.next = &n3;
+	n3.next = &n4;
+	n4.next = NULL;
 
-    Node* tmp;
-    tmp = head;
-    while ((*tmp).next != NULL) {      // do not go inside when tmp = last node
-        printf("%d\n", tmp->data);     // because lastnode.next = NULL
-        tmp = tmp->next;
-    }
-
-    printf("%d\n", tmp->data); 
+	for(Node* tmp = head; tmp != NULL; tmp = tmp->next){
+		printf("%d\n", tmp->data);
+	}
 }
