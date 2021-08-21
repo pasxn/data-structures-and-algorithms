3839272454d974de18abc3e5deeeb95cf61381e0
pasxn <pasansanjula.android@gmail.com>
2021-08-21 16:40:21 +0530

    rebubble

--- assignment_1/bubble_sort.cpp
+++ assignment_1/bubble_sort.cpp
@@ -2,28 +2,23 @@
 #include <vector>
 #include <fstream>
 
-/* function to sort arr using shellSort */
-void shellSort(int arr[], int n) {
-    // Start with a big gap, then reduce the gap
-    for (int gap = n/2; gap > 0; gap /= 2) {
-        // Do a gapped insertion sort for this gap size.
-        // The first gap elements a[0..gap-1] are already in gapped order
-        // keep adding one more element until the entire array is
-        // gap sorted
-        for (int i = gap; i < n; i += 1) {
-            // add a[i] to the elements that have been gap sorted
-            // save a[i] in temp and make a hole at position i
-            int temp = arr[i];
- 
-            // shift earlier gap-sorted elements up until the correct
-            // location for a[i] is found
-            int j;           
-            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
-                arr[j] = arr[j - gap];
-             
-            //  put temp (the original a[i]) in its correct location
-            arr[j] = temp;
+// an optimized version of Bubble Sort
+void bubblesort(int arr[], int n) {
+    int swapped;
+
+    for (int i = 0; i < n - 1; i++) {
+        swapped = 0;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = 1;  
+            }
         }
+        // if no two elements were swapped by inner loop, then break
+        if (swapped == 1)
+                break;
     }
 }
 
@@ -36,10 +31,10 @@ void printarray(int arr[], int n){
 int main(int argc, char** argv) {
 
     std::string loc = argv[1];
-    
+
     std::vector <int> v;
     std::ifstream file(loc);
-    
+
     if (file) {        
         int value;
         while ( file >> value ) 
@@ -52,10 +47,10 @@ int main(int argc, char** argv) {
 
     for (int i = 0; i < n; i++) 
         arr[i] = v[i];
-    
-    printarray(arr, n);
-
-    shellSort(arr, n);
 
     printarray(arr, n);
-}
+
+    bubblesort(arr, n);
+
+    printarray(arr, n);
+} 
